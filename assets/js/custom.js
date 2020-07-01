b9fc17fec4d27b003c0ff5ebac3051f23416bae2
axit <mostakim.shakil@gmail.com>
2020-07-01 16:04:48 +0600

    add a new project

---
+++ assets/js/custom.js
@@ -0,0 +1,32 @@
+ $(document).ready(function(){
+
+     //Start Menu js
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > 550) {
+        $('.navbar-light').addClass('menu-shrink');
+    } else {
+        $('.navbar-light').removeClass('menu-shrink');
+    }
+});			
+$('.navbar-nav li a').on('click', function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top - 60
+    }, 1000);
+    e.preventDefault();
+});
+
+$(document).on('click','.navbar-collapse.show',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});	
+
+// WOW js
+new WOW().init();
+
+
+
+
+ });
+ 
